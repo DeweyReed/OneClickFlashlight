2b4382ab3a030706c1897947c3d615cc3ea296b3
Reed <deweyreed@protonmail.com>
2020-05-28 13:26:48 +0800

    Done

--- app/src/main/java/xyz/aprildown/torch/MainActivity.kt
+++ app/src/main/java/xyz/aprildown/torch/MainActivity.kt
@@ -1,12 +1,35 @@
 package xyz.aprildown.torch
 
+import android.app.Activity
+import android.content.Context
+import android.hardware.camera2.CameraManager
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val cm = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
+        val callback = object : CameraManager.TorchCallback() {
+            override fun onTorchModeChanged(cameraId: String, enabled: Boolean) {
+                cm.unregisterTorchCallback(this)
+
+                for (currentCameraId in cm.cameraIdList) {
+                    try {
+                        cm.setTorchMode(currentCameraId, !enabled)
+                        break
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+
+        cm.registerTorchCallback(callback, null)
+        cm.unregisterTorchCallback(callback)
+
+        finish()
     }
 }
