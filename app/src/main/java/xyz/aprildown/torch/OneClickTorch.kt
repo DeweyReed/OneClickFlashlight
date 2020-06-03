d914532fa13f733a2adbc856b4a0aba95d9e0e49
Reed <deweyreed@protonmail.com>
2020-06-03 11:06:43 +0800

    Add SettingsActivity

---
+++ app/src/main/java/xyz/aprildown/torch/OneClickTorch.kt
@@ -0,0 +1,18 @@
+package xyz.aprildown.torch
+
+import android.app.Application
+import android.os.Build
+import androidx.appcompat.app.AppCompatDelegate
+
+class OneClickTorch : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+            } else {
+                AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY
+            }
+        )
+    }
+}
