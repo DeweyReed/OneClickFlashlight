7915f833ff82d3ddf188fa81fe72dfd4002d0adf
Reed <deweyreed@protonmail.com>
2020-06-04 10:32:17 +0800

    Add BrightScreenActivity

---
+++ app/src/main/java/xyz/aprildown/torch/BrightScreenActivity.kt
@@ -0,0 +1,21 @@
+package xyz.aprildown.torch
+
+import android.app.Activity
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+
+class BrightScreenActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val view = View(this)
+        view.setBackgroundColor(Color.WHITE)
+        setContentView(view)
+
+        window?.run {
+            attributes = attributes.apply { screenBrightness = 1f }
+            statusBarColor = Color.WHITE
+            navigationBarColor = Color.WHITE
+        }
+    }
+}
