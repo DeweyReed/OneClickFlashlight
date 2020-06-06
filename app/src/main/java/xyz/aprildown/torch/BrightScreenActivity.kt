62da51c0e669cdd75b2f43cc127c6823e82b9204
DeweyReed <deweyreed@protonmail.com>
2020-06-06 12:01:05 +0800

    Set BrightScreenActivity window background to transparent to prevent overdraw

--- app/src/main/java/xyz/aprildown/torch/BrightScreenActivity.kt
+++ app/src/main/java/xyz/aprildown/torch/BrightScreenActivity.kt
@@ -8,6 +8,8 @@ import android.view.View
 class BrightScreenActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window?.setBackgroundDrawableResource(android.R.color.transparent)
+
         val view = View(this)
         view.setBackgroundColor(Color.WHITE)
         setContentView(view)
