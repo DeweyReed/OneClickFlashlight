9de81448b6e89dcbbcf6af140929b8206daca85a
DeweyReed <deweyreed@protonmail.com>
2020-08-22 14:01:43 +0800

    Keep hiding system UI while touching the screen

--- app/src/main/java/xyz/aprildown/torch/AntiTouchActivity.kt
+++ app/src/main/java/xyz/aprildown/torch/AntiTouchActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.KeyEvent
+import android.view.MotionEvent
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.DefaultLifecycleObserver
@@ -16,11 +17,22 @@ class AntiTouchActivity : AppCompatActivity() {
         val binding = ActivityAntiTouchBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.root.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-                View.SYSTEM_UI_FLAG_FULLSCREEN
+        val fullScreenSystemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE or
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        fun toFullScreen() {
+            binding.root.systemUiVisibility = fullScreenSystemUiVisibility
+        }
+
+        toFullScreen()
+        window?.decorView?.setOnTouchListener { _, event ->
+            if (event.actionMasked == MotionEvent.ACTION_MOVE) {
+                toFullScreen()
+            }
+            true
+        }
 
         val ordinal = intent?.getIntExtra(EXTRA_TYPE, 0) ?: 0
         val observer = when (FlashlightShortcut.values()[ordinal]) {
